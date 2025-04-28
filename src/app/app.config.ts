46173a2167f28125340587625dbe0c92da31231a
Daniel Sogl <daniel.sogl@thinktecture.com>
2025-04-28 22:11:41 +0200

    refactor(app): enhance router configuration with preloading and HTTP client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { provideRouter } from "@angular/router";
+import { PreloadAllModules, provideRouter, withPreloading } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { provideHttpClient } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
     provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
