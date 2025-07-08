e5a006353e7216671a31e97938d56a38d83ff044
Daniel Sogl <daniel.sogl@thinktecture.com>
2025-07-08 09:13:40 +0200

    chore: use vitest instead of jasmin and karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,25 @@
-import { MockBuilder, MockRender, ngMocks } from "ng-mocks";
+import { provideZonelessChangeDetection } from "@angular/core";
+import type { ComponentFixture } from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
+import { provideRouter } from "@angular/router";
 
 describe("AppComponent", () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
-    await MockBuilder(AppComponent);
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideZonelessChangeDetection(), provideRouter([])],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it("should create the app", () => {
-    MockRender(AppComponent);
-    const app = ngMocks.findInstance(AppComponent);
-    expect(app).toBeTruthy();
+  it("should create", () => {
+    expect(component).toBeTruthy();
   });
 });
