8dc0888742b03f549c18c654352691050f3455d2
Daniel Sogl <me@danielsogl.de>
2025-12-19 08:59:56 +0100

    refactor(config): replace unused providers with native date adapter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,21 +2,19 @@ import {
   ApplicationConfig,
   provideZonelessChangeDetection,
 } from "@angular/core";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import {
   PreloadAllModules,
   provideRouter,
   withPreloading,
 } from "@angular/router";
 
-import { provideHttpClient } from "@angular/common/http";
+import { provideNativeDateAdapter } from "@angular/material/core";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideAnimationsAsync(),
-    provideHttpClient(),
+    provideNativeDateAdapter(),
   ],
 };
